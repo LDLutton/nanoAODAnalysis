2d15b4395f381ac8edf130f4ff8793ef70bdc8f0
LDLutton <lynnelutton@gmail.com>
2022-12-01 20:37:59 -0500

    bug fix

--- DoHLTFilterOnReweightingBeforeAnalysis.cc
+++ DoHLTFilterOnReweightingBeforeAnalysis.cc
@@ -47,7 +47,7 @@
 ////////////////////////////////START OF MAIN FUNCTION////////////////////////////////
 
 
-void DoHLTFilterBeforeAnalysis(UInt_t fileInd){
+void DoHLTFilterOnReweightingBeforeAnalysis(UInt_t fileInd){
     // Open the file. Note that the name of your file outside this class
     // will probably NOT be experiment.root.
     std::cout << "start\n";
