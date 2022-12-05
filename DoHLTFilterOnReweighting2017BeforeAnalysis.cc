414f28fe4501edc57a6fd2eca93bd556fa03e8ab
LDLutton <lynnelutton@gmail.com>
2022-12-05 18:40:49 -0500

    bug fix

--- DoHLTFilterOnReweighting2017BeforeAnalysis.cc
+++ DoHLTFilterOnReweighting2017BeforeAnalysis.cc
@@ -765,7 +765,6 @@ void DoHLTFilterOnReweighting2017BeforeAnalysis(UInt_t fileInd){
 
         TTreeReaderValue<Bool_t> HLT_Ele115_CaloIdVT_GsfTrkIdT(myEventsReader, "HLT_Ele115_CaloIdVT_GsfTrkIdT");
         TTreeReaderValue<Bool_t> HLT_Ele27_WPTight_Gsf(myEventsReader, "HLT_Ele27_WPTight_Gsf");
-        TTreeReaderValue<Bool_t> HLT_Ele28_WPTight_Gsf(myEventsReader, "HLT_Ele28_WPTight_Gsf");
         TTreeReaderValue<Bool_t> HLT_Ele32_WPTight_Gsf(myEventsReader, "HLT_Ele32_WPTight_Gsf");
         TTreeReaderValue<Bool_t> HLT_Ele35_WPTight_Gsf(myEventsReader, "HLT_Ele35_WPTight_Gsf");
         TTreeReaderValue<Bool_t> HLT_Ele38_WPTight_Gsf(myEventsReader, "HLT_Ele38_WPTight_Gsf");
